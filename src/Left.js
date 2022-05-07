eb1d64675d280ce0c3fbc16e518a7777bec56d9e
J040V1T0R <joaovitor.jovg73@gmail.com>
2022-05-07 17:51:23 -0300

    Refatoramento concluido

--- src/Left.js
+++ src/Left.js
@@ -33,6 +33,25 @@ const storyDados = [
     }
 ]
 
+const postDados = [
+    {
+        imageUserPost: "mediafiles/instagram/assets/img/meowed.svg",
+        nameUserPost: "meowed",
+        imagePost: "mediafiles/instagram/assets/img/gato-telefone.svg",
+        likeUserImage: "mediafiles/instagram/assets/img/respondeai.svg",
+        likeUser: "respondeai",
+        totalPeopleLike: "101.523",
+    },
+    {
+        imageUserPost: "mediafiles/instagram/assets/img/barked.svg",
+        nameUserPost: "barked",
+        imagePost: "mediafiles/instagram/assets/img/dog.svg",
+        likeUserImage: "mediafiles/instagram/assets/img/adorable_animals.svg",
+        likeUser: "adorable_animals",
+        totalPeopleLike: "99.159",
+    },
+]
+
 function Story (props) {
     return (
         <div class="story">
@@ -46,25 +65,6 @@ function Story (props) {
 
     )
 }
-
-const postDados = [
-    {
-        imageUserPost: "mediafiles/instagram/assets/img/meowed.svg",
-        nameUserPost: "meowed",
-        imagePost: "mediafiles/instagram/assets/img/gato-telefone.svg",
-        likeUserImage: "assets/img/respondeai.svg",
-        likeUser: "respondeai",
-        totalPeopleLike: "101.523",
-    },
-    {
-        imageUserPost: "assets/img/barked.svg",
-        nameUserPost: "barked",
-        imagePost: "assets/img/dog.svg",
-        likeUserImage: "assets/img/adorable_animals.svg",
-        likeUser: "adorable_animals",
-        totalPeopleLike: "99.159",
-    },
-]
 
 function Post (props) {
     return (
@@ -106,9 +106,6 @@ function Post (props) {
     )
 }
 
-
-
-
 export default function Left () {
     return (
         <div class="esquerda">
@@ -119,90 +116,14 @@ export default function Left () {
                 <div class="setinha">
                     <ion-icon name="chevron-forward-circle"></ion-icon>
                 </div>
-          </div>
+             </div>
 
             <div class="posts">
                     {postDados.map((url) => (
                         <Post imageUserPost={url.imageUserPost} nameUserPost={url.nameUserPost} imagePost={url.imagePost} likeUserImage={url.likeUserImage} likeUser={url.likeUser} totalPeopleLike={url.totalPeopleLike} />
                     ))}
 
-
-
-
-
-
-{/*               <div class="post">
-                    <div class="topo">
-                    <div class="usuario">
-                        <img src="mediafiles/instagram/assets/img/meowed.svg" />
-                        meowed
-                    </div>
-                    <div class="acoes">
-                        <ion-icon name="ellipsis-horizontal"></ion-icon>
-                    </div>
-                    </div>
-
-                    <div class="conteudo">
-                    <img src="mediafiles/instagram/assets/img/gato-telefone.svg" />
-                    </div>
-
-                    <div class="fundo">
-                    <div class="acoes">
-                        <div>
-                        <ion-icon name="heart-outline"></ion-icon>
-                        <ion-icon name="chatbubble-outline"></ion-icon>
-                        <ion-icon name="paper-plane-outline"></ion-icon>
-                        </div>
-                        <div>
-                        <ion-icon name="bookmark-outline"></ion-icon>
-                        </div>
-                    </div>
-
-                    <div class="curtidas">
-                        <img src="mediafiles/instagram/assets/img/respondeai.svg" />
-                        <div class="texto">
-                        Curtido por <strong>respondeai</strong> e <strong>outras 101.523 pessoas</strong>
-                        </div>
-                    </div>
-                    </div>
-                </div>
-
-                <div class="post">
-                    <div class="topo">
-                    <div class="usuario">
-                        <img src="mediafiles/instagram/assets/img/barked.svg" />
-                        barked
-                    </div>
-                    <div class="acoes">
-                        <ion-icon name="ellipsis-horizontal"></ion-icon>
-                    </div>
-                    </div>
-
-                    <div class="conteudo">
-                    <img src="mediafiles/instagram/assets/img/dog.svg" />
-                    </div>
-
-                    <div class="fundo">
-                    <div class="acoes">
-                        <div>
-                        <ion-icon name="heart-outline"></ion-icon>
-                        <ion-icon name="chatbubble-outline"></ion-icon>
-                        <ion-icon name="paper-plane-outline"></ion-icon>
-                        </div>
-                        <div>
-                        <ion-icon name="bookmark-outline"></ion-icon>
-                        </div>
-                    </div>
-
-                    <div class="curtidas">
-                        <img src="mediafiles/instagram/assets/img/adorable_animals.svg" />
-                        <div class="texto">
-                        Curtido por <strong>adorable_animals</strong> e <strong>outras 99.159 pessoas</strong>
-                        </div>
-                    </div>
-                    </div>
-                </div>
-*/}             </div>
+            </div>
         </div>
     )
 }
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,87 +1,75 @@
+const sugestoesDados = [
+    {
+        user: "bad.vibes.memes",
+        image: "mediafiles/instagram/assets/img/bad.vibes.memes.svg"
+    },
+    {
+        user: "chibirdart",
+        image: "mediafiles/instagram/assets/img/chibirdart.svg"
+    },
+    {
+        user: "razoesparaacreditar",
+        image: "mediafiles/instagram/assets/img/razoesparaacreditar.svg"
+    },
+    {
+        user: "adorable_animals",
+        image: "mediafiles/instagram/assets/img/adorable_animals.svg"
+    },
+    {
+        user: "smallcutecats",
+        image: "mediafiles/instagram/assets/img/smallcutecats.svg"
+    },
+]
+
+function Sugetao (props) {
+    return (
+        <div class="sugestao">
+            <div class="usuario">
+                <img src={props.image} />
+                <div class="texto">
+                    <div class="nome">{props.user}</div>
+                    <div class="razao">Segue você</div>
+                </div>
+            </div>
+
+            <div class="seguir">Seguir</div>
+        </div>
+    )
+}
+
+function User (props) {
+    return (
+        <div class="usuario">
+            <img src={props.userImage} />
+            <div class="texto">
+                <strong>{props.userNameCaps}</strong>
+                {props.userNameLown}
+            </div>
+        </div>
+    )
+}
+
 export default function Sidebar () {
     return (
         <div class="sidebar">
-            <div class="usuario">
-            <img src="mediafiles/instagram/assets/img/catanacomics.svg" />
-            <div class="texto">
-                <strong>catanacomics</strong>
-                Catana
-            </div>
-            </div>
+            <User userImage="mediafiles/instagram/assets/img/catanacomics.svg" userNameCaps="catanacomics" userNameLown="Catana" />
 
             <div class="sugestoes">
-            <div class="titulo">
-                Sugestões para você
-                <div>Ver tudo</div>
-            </div>
-
-            <div class="sugestao">
-                <div class="usuario">
-                <img src="mediafiles/instagram/assets/img/bad.vibes.memes.svg" />
-                <div class="texto">
-                    <div class="nome">bad.vibes.memes</div>
-                    <div class="razao">Segue você</div>
+                <div class="titulo">
+                    Sugestões para você
+                    <div>Ver tudo</div>
                 </div>
-                </div>
-
-                <div class="seguir">Seguir</div>
-            </div>
-
-            <div class="sugestao">
-                <div class="usuario">
-                <img src="mediafiles/instagram/assets/img/chibirdart.svg" />
-                <div class="texto">
-                    <div class="nome">chibirdart</div>
-                    <div class="razao">Segue você</div>
-                </div>
-                </div>
-
-                <div class="seguir">Seguir</div>
-            </div>
-
-            <div class="sugestao">
-                <div class="usuario">
-                <img src="mediafiles/instagram/assets/img/razoesparaacreditar.svg" />
-                <div class="texto">
-                    <div class="nome">razoesparaacreditar</div>
-                    <div class="razao">Novo no Instagram</div>
-                </div>
-                </div>
-
-                <div class="seguir">Seguir</div>
-            </div>
-
-            <div class="sugestao">
-                <div class="usuario">
-                <img src="mediafiles/instagram/assets/img/adorable_animals.svg" />
-                <div class="texto">
-                    <div class="nome">adorable_animals</div>
-                    <div class="razao">Segue você</div>
-                </div>
-                </div>
-
-                <div class="seguir">Seguir</div>
-            </div>
-
-            <div class="sugestao">
-                <div class="usuario">
-                <img src="mediafiles/instagram/assets/img/smallcutecats.svg" />
-                <div class="texto">
-                    <div class="nome">smallcutecats</div>
-                    <div class="razao">Segue você</div>
-                </div>
-                </div>
-
-                <div class="seguir">Seguir</div>
-            </div>
+                {sugestoesDados.map((newArray) => (
+                    <Sugetao image={newArray.image} user={newArray.user} />
+                ))}
             </div>
 
             <div class="links">
-            Sobre • Ajuda • Imprensa • API • Carreiras • Privacidade • Termos • Localizações • Contas mais relevantes • Hashtags • Idioma
+                Sobre • Ajuda • Imprensa • API • Carreiras • Privacidade • Termos • Localizações • Contas mais relevantes • Hashtags • Idioma
             </div>
 
             <div class="copyright">
-            © 2021 INSTAGRAM DO FACEBOOK
+                © 2021 INSTAGRAM DO FACEBOOK
             </div>
         </div>
     )
